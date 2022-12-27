28fed27b8d995fc30c57977f29ca9a2ceceb9ea2
schneond <ondrej.schneider@tietoevry.com>
2022-12-27 03:13:40 +0100

    Update ExampleMod.java

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,13 +1,22 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.ClientTickCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.LiteralText;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -27,5 +36,16 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+
+		ClientCommandRegistrationCallback ClientTickCallback = null;
+		ClientTickCallback.EVENT.register((client) -> {
+			PlayerEntity player = MinecraftClient.getInstance().player;
+			ClientPlayerEntity player = client.player;
+			if (player != null) {
+				player.setDisplayName(new LiteralText("[Přípona] " + player.getName().getString()));
+			}
+		});
 	}
+
 }
